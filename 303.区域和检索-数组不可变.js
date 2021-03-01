4578b6c6d2fdfc12a355840b8b860a13761b4de9
scarsu <scarsu001@gmail.com>
2021-03-01 09:17:21 +0800

    #303 easy 前缀和

---
+++ 303.区域和检索-数组不可变.js
@@ -0,0 +1,93 @@
+/*
+ * @lc app=leetcode.cn id=303 lang=javascript
+ *
+ * [303] 区域和检索 - 数组不可变
+ *
+ * https://leetcode-cn.com/problems/range-sum-query-immutable/description/
+ *
+ * algorithms
+ * Easy (63.75%)
+ * Likes:    255
+ * Dislikes: 0
+ * Total Accepted:    71.8K
+ * Total Submissions: 109K
+ * Testcase Example:  '["NumArray","sumRange","sumRange","sumRange"]\n[[[-2,0,3,-5,2,-1]],[0,2],[2,5],[0,5]]'
+ *
+ * 给定一个整数数组  nums，求出数组从索引 i 到 j（i ≤ j）范围内元素的总和，包含 i、j 两点。
+ * 
+ * 
+ * 
+ * 实现 NumArray 类：
+ * 
+ * 
+ * NumArray(int[] nums) 使用数组 nums 初始化对象
+ * int sumRange(int i, int j) 返回数组 nums 从索引 i 到 j（i ≤ j）范围内元素的总和，包含 i、j 两点（也就是
+ * sum(nums[i], nums[i + 1], ... , nums[j])）
+ * 
+ * 
+ * 
+ * 
+ * 示例：
+ * 
+ * 
+ * 输入：
+ * ["NumArray", "sumRange", "sumRange", "sumRange"]
+ * [[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
+ * 输出：
+ * [null, 1, -1, -3]
+ * 
+ * 解释：
+ * NumArray numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
+ * numArray.sumRange(0, 2); // return 1 ((-2) + 0 + 3)
+ * numArray.sumRange(2, 5); // return -1 (3 + (-5) + 2 + (-1)) 
+ * numArray.sumRange(0, 5); // return -3 ((-2) + 0 + 3 + (-5) + 2 + (-1))
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 0 
+ * -10^5 
+ * 0 
+ * 最多调用 10^4 次 sumRange 方法
+ * 
+ * 
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ */
+var NumArray = function(nums) {
+  this.nums=nums
+  // 计算前缀和，优化sumRange方法，复杂度从累加的O(N)变为O(1)
+  this.sums=[]
+  nums.forEach((e,i)=>{
+    if(i===0){
+      this.sums[i]=e
+    }else{
+      this.sums[i]=this.sums[i-1]+e
+    }
+  })
+};
+
+/** 
+ * @param {number} i 
+ * @param {number} j
+ * @return {number}
+ */
+NumArray.prototype.sumRange = function(i, j) {
+  return this.sums[j]-(i===0?0:this.sums[i-1])
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = new NumArray(nums)
+ * var param_1 = obj.sumRange(i,j)
+ */
+// @lc code=end
+
