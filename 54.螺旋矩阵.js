183175159098bfbc657f36c13c1e3b220e9c92eb
scarsu <scarsu001@gmail.com>
2021-03-15 11:21:13 +0800

    #54 medium 螺旋遍历矩阵 模拟 按层模拟

--- 54.螺旋矩阵.js
+++ 54.螺旋矩阵.js
@@ -45,49 +45,84 @@
  */
 
 // @lc code=start
-/**
- * @param {number[][]} matrix
- * @return {number[]}
- */
-var spiralOrder = function(matrix) {
-  // 模拟
+// 模拟
+// var spiralOrder = function(matrix) {
+//   const row = matrix.length
+//   const col = matrix[0].length
+//   const total = row*col
+//   const ret = []
+//   // 用相同尺寸的矩阵作为元素访问标识
+//   const visited = Array(row).fill(0).map(i=>new Array(col).fill(false))
+//   // 螺旋的四个方向下 每次遍历下个节点[x,y]的索引增量
+//   const directions = [[0,1],[1,0],[0,-1],[-1,0]] // 
 
+//   let x = 0,y = 0
+//   let directionIndex = 0
+//   while(ret.length<total){
+//     // 当前节点
+//     let cur = matrix[x][y]
+//     visited[x][y] = true
+//     ret.push(cur)
+//     // 转换方向判断
+//     const nextX = x + directions[directionIndex][0];
+//     const nextY = y + directions[directionIndex][1];
+//     if(
+//       !(
+//         nextX >= 0 &&
+//         nextX < row &&
+//         nextY >=0 &&
+//         nextY < col &&
+//         !visited[nextX][nextY]
+//       )
+//     ){
+//       directionIndex = (directionIndex + 1) % 4
+//     }
+//     // 下一节点
+//     x += directions[directionIndex][0];
+//     y += directions[directionIndex][1];
+//   }
+
+//   return ret
+// };
+
+
+
+// 按层模拟
+var spiralOrder = function(matrix) {
   const row = matrix.length
   const col = matrix[0].length
   const total = row*col
   const ret = []
-  // 用相同尺寸的矩阵作为元素访问标识
-  const visited = Array(row).fill(0).map(i=>new Array(col).fill(false))
-  // 螺旋的四个方向下 每次遍历下个节点[x,y]的索引增量
-  const directions = [[0,1],[1,0],[0,-1],[-1,0]] // 
 
-  let x = 0,y = 0
-  let directionIndex = 0
-  while(ret.length<total){
-    // 当前节点
-    let cur = matrix[x][y]
-    visited[x][y] = true
-    ret.push(cur)
-    // 转换方向判断
-    const nextX = x + directions[directionIndex][0];
-    const nextY = y + directions[directionIndex][1];
-    if(
-      !(
-        nextX >= 0 &&
-        nextX < row &&
-        nextY >=0 &&
-        nextY < col &&
-        !visited[nextX][nextY]
-      )
-    ){
-      directionIndex = (directionIndex + 1) % 4
+  // 每一层的四个顶点索引
+  let left=0;right=col-1;top=0;bottom=row-1
+  while(left<=right && top<=bottom){
+    // 左上>右上 [top,left] [top,right]
+    for(let x=left;x<=right;x++){
+      ret.push(matrix[top][x])
     }
-    // 下一节点
-    x += directions[directionIndex][0];
-    y += directions[directionIndex][1];
+    // 右上>右下 [top+1,right] [bottom,right]
+    for(let x=top+1;x<=bottom;x++){
+      ret.push(matrix[x][right])
+    }
+    // 右下>左下 [bottom,right-1] [bottom,left]
+    if(top<bottom){
+      for(let x=right-1;x>=left;x--){
+        ret.push(matrix[bottom][x])
+      }
+
+    }
+    // 左下>左上 [bottom-1,left] [top+1,left]
+    if(top<bottom&&left<right){
+      for(let x=bottom-1;x>=top+1;x--){
+        ret.push(matrix[x][left])
+      }
+    }
+    // 计算下一层的四个顶点索引
+    [left,right,top,bottom]=[left+1,right-1,top+1,bottom-1]
   }
 
   return ret
-};
+}
 // @lc code=end
 
