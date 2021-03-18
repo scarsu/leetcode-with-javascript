26b1aa24baaf941d399b65565f78459e18c4be42
scarsu <scarsu001@gmail.com>
2021-03-18 17:50:09 +0800

    #92 medium   反转链表   链表截取&拼接

---
+++ 92.反转链表-ii.js
@@ -0,0 +1,105 @@
+/*
+ * @lc app=leetcode.cn id=92 lang=javascript
+ *
+ * [92] 反转链表 II
+ *
+ * https://leetcode-cn.com/problems/reverse-linked-list-ii/description/
+ *
+ * algorithms
+ * Medium (51.68%)
+ * Likes:    790
+ * Dislikes: 0
+ * Total Accepted:    130.7K
+ * Total Submissions: 244.2K
+ * Testcase Example:  '[1,2,3,4,5]\n2\n4'
+ *
+ * 给你单链表的头指针 head 和两个整数 left 和 right ，其中 left  。请你反转从位置 left 到位置 right 的链表节点，返回
+ * 反转后的链表 。
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：head = [1,2,3,4,5], left = 2, right = 4
+ * 输出：[1,4,3,2,5]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：head = [5], left = 1, right = 1
+ * 输出：[5]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 链表中节点数目为 n
+ * 1 
+ * -500 
+ * 1 
+ * 
+ * 
+ * 
+ * 
+ * 进阶： 你可以使用一趟扫描完成反转吗？
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+var reverseBetween = function(head, left, right) {
+  // 因为头节点有可能发生变化，使用虚拟头节点可以避免复杂的分类讨论
+  const dummyNode = new ListNode(-1);
+  dummyNode.next = head;
+
+  let pre = dummyNode;
+  // 第 1 步：从虚拟头节点走 left - 1 步，来到 left 节点的前一个节点
+  for (let i = 0; i < left - 1; i++) {
+      pre = pre.next;
+  }
+  // 第 2 步：从 pre 再走 right - left + 1 步，来到 right 节点
+  let rightNode = pre;
+  for (let i = 0; i < right - left + 1; i++) {
+      rightNode = rightNode.next;
+  }
+
+  // 第 3 步：切断出一个子链表（截取链表）
+  let leftNode = pre.next;
+  let curr = rightNode.next;
+
+  // 注意：切断链接
+  pre.next = null;
+  rightNode.next = null;
+
+  // 第 4 步：同第 206 题，反转链表的子区间
+  reverseLinkedList(leftNode);
+
+  // 第 5 步：接回到原来的链表中
+  pre.next = rightNode;
+  leftNode.next = curr;
+  return dummyNode.next;
+};
+
+// 翻转链表
+const reverseLinkedList = (head) => {
+let pre = null;
+let cur = head;
+
+while (cur) {
+  const next = cur.next;  // 拿到下一节点
+  cur.next = pre; // 真正的翻转：把当前节点的next指向上一节点
+  pre = cur;  // 为下一次循环移动pre指针
+  cur = next; // 为下一次循环移动cur指针
+}
+}
+// @lc code=end
+
