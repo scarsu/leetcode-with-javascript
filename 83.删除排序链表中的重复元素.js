0fea0242380174c47d26a2130db85357a8e8b065
scarsu <scarsu001@gmail.com>
2021-03-26 09:22:03 +0800

    #83 easy 删除排序链表重复元素

---
+++ 83.删除排序链表中的重复元素.js
@@ -0,0 +1,83 @@
+/*
+ * @lc app=leetcode.cn id=83 lang=javascript
+ *
+ * [83] 删除排序链表中的重复元素
+ *
+ * https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list/description/
+ *
+ * algorithms
+ * Easy (51.62%)
+ * Likes:    511
+ * Dislikes: 0
+ * Total Accepted:    206.6K
+ * Total Submissions: 392.6K
+ * Testcase Example:  '[1,1,2]'
+ *
+ * 存在一个按升序排列的链表，给你这个链表的头节点 head ，请你删除所有重复的元素，使每个元素 只出现一次 。
+ * 
+ * 返回同样按升序排列的结果链表。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：head = [1,1,2]
+ * 输出：[1,2]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：head = [1,1,2,3,3]
+ * 输出：[1,2,3]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 链表中节点数目在范围 [0, 300] 内
+ * -100 
+ * 题目数据保证链表已经按升序排列
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+  if(!head) return head
+  let cur=head
+  while(cur.next){
+    if(cur.val===cur.next.val){
+      let x=cur.val
+      while(cur.next && cur.next.val===x){
+        cur.next=cur.next.next
+      }
+    }else{
+      cur=cur.next
+    }
+  }
+  return head
+};
+// @lc code=end
+
