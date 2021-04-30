67e38e376b09c459afebf8fb465665420626ae11
scarsu <scarsu001@gmail.com>
2021-04-30 11:42:47 +0800

    #66 easy 遍历

---
+++ 66.加一.js
@@ -0,0 +1,84 @@
+/*
+ * @lc app=leetcode.cn id=66 lang=javascript
+ *
+ * [66] 加一
+ *
+ * https://leetcode-cn.com/problems/plus-one/description/
+ *
+ * algorithms
+ * Easy (45.71%)
+ * Likes:    683
+ * Dislikes: 0
+ * Total Accepted:    283.3K
+ * Total Submissions: 619.8K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * 给定一个由 整数 组成的 非空 数组所表示的非负整数，在该数的基础上加一。
+ * 
+ * 最高位数字存放在数组的首位， 数组中每个元素只存储单个数字。
+ * 
+ * 你可以假设除了整数 0 之外，这个整数不会以零开头。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：digits = [1,2,3]
+ * 输出：[1,2,4]
+ * 解释：输入数组表示数字 123。
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：digits = [4,3,2,1]
+ * 输出：[4,3,2,2]
+ * 解释：输入数组表示数字 4321。
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：digits = [0]
+ * 输出：[1]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 
+ * 0 
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+  const n=digits.length
+  let cur=n-1,carry=1
+  while(carry>0){
+    if(cur<0){
+      digits.unshift(1)
+      return digits
+    }
+    let sum=digits[cur]+carry
+    if(sum<10){
+      digits[cur]=sum
+      return digits
+    }else{
+      digits[cur]=sum%10
+      carry=1
+      cur--
+    }
+  }
+  return digits
+};
+// @lc code=end
+
