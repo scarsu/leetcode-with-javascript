ab3eb7640102070fcfd0f50ce1f8523320dc9468
scarsu <scarsu001@gmail.com>
2021-05-15 09:43:29 +0800

    #206 easy 反转链表

---
+++ 206.反转链表.js
@@ -0,0 +1,95 @@
+/*
+ * @lc app=leetcode.cn id=206 lang=javascript
+ *
+ * [206] 反转链表
+ *
+ * https://leetcode-cn.com/problems/reverse-linked-list/description/
+ *
+ * algorithms
+ * Easy (70.88%)
+ * Likes:    1737
+ * Dislikes: 0
+ * Total Accepted:    544.7K
+ * Total Submissions: 760.1K
+ * Testcase Example:  '[1,2,3,4,5]'
+ *
+ * 给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+ * 
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：head = [1,2,3,4,5]
+ * 输出：[5,4,3,2,1]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：head = [1,2]
+ * 输出：[2,1]
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：head = []
+ * 输出：[]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 链表中节点的数目范围是 [0, 5000]
+ * -5000 
+ * 
+ * 
+ * 
+ * 
+ * 进阶：链表可以选用迭代或递归方式完成反转。你能否用两种方法解决这道题？
+ * 
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+  // 前置一个哨兵节点
+  let prev=null
+  let cur=head
+  // 在每个节点都翻转方向
+  while(cur){
+    // 记录next节点
+    let next=cur.next
+    // 翻转
+    cur.next=prev
+    // 推进指针
+    prev=cur
+    cur=next
+  }
+  return prev
+};
+// @lc code=end
+
