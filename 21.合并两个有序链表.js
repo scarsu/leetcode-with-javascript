38d43da09cf0f99d335a433a8a7ec9e14629e775
scarsu <scarsu001@gmail.com>
2021-05-24 10:03:47 +0800

    #21 easy 合并有序链表 递归

--- 21.合并两个有序链表.js
+++ 21.合并两个有序链表.js
@@ -47,37 +47,39 @@
  */
 
 // 1迭代
-var mergeTwoLists = function(l1, l2) {
-  let pre=new ListNode(-1)
-  let head=pre
-  while(l1!==null && l2!==null){
-    if(l1.val<l2.val){
-      pre.next=l1 // 指定下一节点
-      l1=l1.next // 移动向下一个节点
-    }else{
-      pre.next=l2 // 指定下一节点
-      l2=l2.next // 移动向下一个节点
-    }
-    pre=pre.next // 移动向下一个节点
-  }
-  pre.next=l1===null?l2:l1
-  return head.next
-};
+// var mergeTwoLists = function(l1, l2) {
+//   let pre=new ListNode(-1)
+//   let head=pre
+//   while(l1!==null && l2!==null){
+//     if(l1.val<l2.val){
+//       pre.next=l1 // 指定下一节点
+//       l1=l1.next // 移动向下一个节点
+//     }else{
+//       pre.next=l2 // 指定下一节点
+//       l2=l2.next // 移动向下一个节点
+//     }
+//     pre=pre.next // 移动向下一个节点
+//   }
+//   pre.next=l1===null?l2:l1
+//   return head.next
+// };
 
 // 递归
-// var mergeTwoLists = function(l1, l2) {
-//   if(l1===null){
-//     return l2
-//   }else if(l2===null){
-//     return l1
-//   }else if(l1.val<l2.val){
-//     l1.next=mergeTwoLists(l1.next,l2)
-//     return l1 
-//   }else{
-//     l2.next=mergeTwoLists(l2.next,l1)
-//     return l2
-//   }
-// };
+// 每次取两个链表头中  较小的一个
+// 终止条件：某一个链表为null
+var mergeTwoLists = function(l1, l2) {
+  if(l1===null){
+    return l2
+  }else if(l2===null){
+    return l1
+  }else if(l1.val<l2.val){
+    l1.next=mergeTwoLists(l1.next,l2)
+    return l1 
+  }else{
+    l2.next=mergeTwoLists(l2.next,l1)
+    return l2
+  }
+};
 
 // @lc code=end
 
