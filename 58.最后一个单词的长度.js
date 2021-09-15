db50af017cb7c367b3af27d029d67258f85f69f3
scarsu <scarsu001@gmail.com>
2021-09-15 09:26:12 +0800

    #58 easy

---
+++ 58.最后一个单词的长度.js
@@ -0,0 +1,64 @@
+/*
+ * @lc app=leetcode.cn id=58 lang=javascript
+ *
+ * [58] 最后一个单词的长度
+ *
+ * https://leetcode-cn.com/problems/length-of-last-word/description/
+ *
+ * algorithms
+ * Easy (35.52%)
+ * Likes:    350
+ * Dislikes: 0
+ * Total Accepted:    219.2K
+ * Total Submissions: 616K
+ * Testcase Example:  '"Hello World"'
+ *
+ * 给你一个字符串 s，由若干单词组成，单词前后用一些空格字符隔开。返回字符串中最后一个单词的长度。
+ * 
+ * 单词 是指仅由字母组成、不包含任何空格字符的最大子字符串。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：s = "Hello World"
+ * 输出：5
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：s = "   fly me   to   the moon  "
+ * 输出：4
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：s = "luffy is still joyboy"
+ * 输出：6
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= s.length <= 10^4
+ * s 仅有英文字母和空格 ' ' 组成
+ * s 中至少存在一个单词
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+  return s.trim().split(' ').pop().length
+};
+// @lc code=end
+
