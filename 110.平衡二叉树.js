6870e1e7bc5d7b50219a867a31dcbad2767b671b
scarsu <scarsu001@gmail.com>
2021-09-30 10:05:39 +0800

    #110 easy 平衡二叉树 dfs

---
+++ 110.平衡二叉树.js
@@ -0,0 +1,85 @@
+/*
+ * @lc app=leetcode.cn id=110 lang=javascript
+ *
+ * [110] 平衡二叉树
+ *
+ * https://leetcode-cn.com/problems/balanced-binary-tree/description/
+ *
+ * algorithms
+ * Easy (56.30%)
+ * Likes:    776
+ * Dislikes: 0
+ * Total Accepted:    256.7K
+ * Total Submissions: 455.7K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * 给定一个二叉树，判断它是否是高度平衡的二叉树。
+ * 
+ * 本题中，一棵高度平衡二叉树定义为：
+ * 
+ * 
+ * 一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1 。
+ * 
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：root = [3,9,20,null,null,15,7]
+ * 输出：true
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：root = [1,2,2,3,3,null,null,4,4]
+ * 输出：false
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：root = []
+ * 输出：true
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 树中的节点数在范围 [0, 5000] 内
+ * -10^4 
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isBalanced = function(root) {
+  const dfs=(node,prevH=0)=>{
+      if(node){
+          const {h:lh,balance:lb}=dfs(node.left,prevH+1)
+          const {h:rh,balance:rb}=dfs(node.right,prevH+1)
+          const balance=lb&&rb&&Math.abs(lh-rh)<=1
+          const h=Math.max(lh,rh)
+          return {h,balance}
+      }
+      return {h:prevH, balance:true}
+  }
+  return dfs(root).balance
+};
+// @lc code=end
+
