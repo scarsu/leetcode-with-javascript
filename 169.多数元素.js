f7832f693ccf70d4775b89d09226026bcf344929
scarsu <scarsu001@gmail.com>
2021-10-09 16:57:11 +0800

    #169 easy 摩尔投票法 对拼消耗

---
+++ 169.多数元素.js
@@ -0,0 +1,68 @@
+/*
+ * @lc app=leetcode.cn id=169 lang=javascript
+ *
+ * [169] 多数元素
+ *
+ * https://leetcode-cn.com/problems/majority-element/description/
+ *
+ * algorithms
+ * Easy (66.47%)
+ * Likes:    1159
+ * Dislikes: 0
+ * Total Accepted:    390.8K
+ * Total Submissions: 588K
+ * Testcase Example:  '[3,2,3]'
+ *
+ * 给定一个大小为 n 的数组，找到其中的多数元素。多数元素是指在数组中出现次数 大于 ⌊ n/2 ⌋ 的元素。
+ * 
+ * 你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：[3,2,3]
+ * 输出：3
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：[2,2,1,1,1,2,2]
+ * 输出：2
+ * 
+ * 
+ * 
+ * 
+ * 进阶：
+ * 
+ * 
+ * 尝试设计时间复杂度为 O(n)、空间复杂度为 O(1) 的算法解决此问题。
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+  // 摩尔投票法 对拼消耗
+  let cur=nums[0]
+  let count=1
+  for(let i=1;i<nums.length;i++){
+      if(nums[i]===cur){
+          count++
+      }else{
+          count--
+          if(count<0) {
+              cur=nums[i]
+              count=1
+          }
+      }
+  }
+  return cur
+};
+// @lc code=end
+
