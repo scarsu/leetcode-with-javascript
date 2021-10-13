551cc2d33aaaa95ea386588bb8d6e8d141058cee
scarsu <scarsu001@gmail.com>
2021-10-13 08:59:55 +0800

    #412 easy

---
+++ 412.fizz-buzz.js
@@ -0,0 +1,64 @@
+/*
+ * @lc app=leetcode.cn id=412 lang=javascript
+ *
+ * [412] Fizz Buzz
+ *
+ * https://leetcode-cn.com/problems/fizz-buzz/description/
+ *
+ * algorithms
+ * Easy (67.35%)
+ * Likes:    114
+ * Dislikes: 0
+ * Total Accepted:    78.2K
+ * Total Submissions: 115K
+ * Testcase Example:  '3'
+ *
+ * 写一个程序，输出从 1 到 n 数字的字符串表示。
+ * 
+ * 1. 如果 n 是3的倍数，输出“Fizz”；
+ * 
+ * 2. 如果 n 是5的倍数，输出“Buzz”；
+ * 
+ * 3.如果 n 同时是3和5的倍数，输出 “FizzBuzz”。
+ * 
+ * 示例：
+ * 
+ * n = 15,
+ * 
+ * 返回:
+ * [
+ * ⁠   "1",
+ * ⁠   "2",
+ * ⁠   "Fizz",
+ * ⁠   "4",
+ * ⁠   "Buzz",
+ * ⁠   "Fizz",
+ * ⁠   "7",
+ * ⁠   "8",
+ * ⁠   "Fizz",
+ * ⁠   "Buzz",
+ * ⁠   "11",
+ * ⁠   "Fizz",
+ * ⁠   "13",
+ * ⁠   "14",
+ * ⁠   "FizzBuzz"
+ * ]
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function(n) {
+  return new Array(n).fill(0).map((e,i)=>{
+      if((i+1)%15===0) return 'FizzBuzz'
+      if((i+1)%5===0) return 'Buzz'
+      if((i+1)%3===0) return 'Fizz'
+      return i+1+''
+  })
+};
+// @lc code=end
+
