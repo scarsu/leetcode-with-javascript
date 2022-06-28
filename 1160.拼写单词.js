9bf53643cc9f17d6acd998553501d711ec113c09
scarsu <scarsu001@gmail.com>
2022-06-28 09:58:47 +0800

    #1160 easy 哈希表计数

---
+++ 1160.拼写单词.js
@@ -0,0 +1,80 @@
+/*
+ * @lc app=leetcode.cn id=1160 lang=javascript
+ *
+ * [1160] 拼写单词
+ *
+ * https://leetcode-cn.com/problems/find-words-that-can-be-formed-by-characters/description/
+ *
+ * algorithms
+ * Easy (68.74%)
+ * Likes:    162
+ * Dislikes: 0
+ * Total Accepted:    66.2K
+ * Total Submissions: 96.7K
+ * Testcase Example:  '["cat","bt","hat","tree"]\n"atach"'
+ *
+ * 给你一份『词汇表』（字符串数组） words 和一张『字母表』（字符串） chars。
+ * 
+ * 假如你可以用 chars 中的『字母』（字符）拼写出 words 中的某个『单词』（字符串），那么我们就认为你掌握了这个单词。
+ * 
+ * 注意：每次拼写（指拼写词汇表中的一个单词）时，chars 中的每个字母都只能用一次。
+ * 
+ * 返回词汇表 words 中你掌握的所有单词的 长度之和。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 输入：words = ["cat","bt","hat","tree"], chars = "atach"
+ * 输出：6
+ * 解释： 
+ * 可以形成字符串 "cat" 和 "hat"，所以答案是 3 + 3 = 6。
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 输入：words = ["hello","world","leetcode"], chars = "welldonehoneyr"
+ * 输出：10
+ * 解释：
+ * 可以形成字符串 "hello" 和 "world"，所以答案是 5 + 5 = 10。
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= words.length <= 1000
+ * 1 <= words[i].length, chars.length <= 100
+ * 所有字符串中都仅包含小写英文字母
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {string[]} words
+ * @param {string} chars
+ * @return {number}
+ */
+var countCharacters = function(words, chars) {
+  const map=new Array(26).fill(0)
+  for(let i=0;i<chars.length;i++){
+      map[chars[i].charCodeAt(0)-'a'.charCodeAt(0)]++
+  }
+
+  const isMaster =word=>{
+      const curMap=[...map]
+      for(let i=0;i<word.length;i++){
+          const index=word[i].charCodeAt(0)-'a'.charCodeAt(0)
+          curMap[index]--
+          if(curMap[index]<0) return false
+      }
+      return true
+  }
+  return words.reduce((p,c)=>{
+      return p+(isMaster(c)?c.length:0)
+  },0)
+};
+// @lc code=end
+
