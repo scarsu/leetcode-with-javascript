e24b2b1f4e31676da03a0a98b6c1a20386229227
scarsu <scarsu001@gmail.com>
2022-07-18 12:13:18 +0800

    #1470 easy

---
+++ 1470.重新排列数组.js
@@ -0,0 +1,68 @@
+/*
+ * @lc app=leetcode.cn id=1470 lang=javascript
+ *
+ * [1470] 重新排列数组
+ *
+ * https://leetcode-cn.com/problems/shuffle-the-array/description/
+ *
+ * algorithms
+ * Easy (84.35%)
+ * Likes:    90
+ * Dislikes: 0
+ * Total Accepted:    61.1K
+ * Total Submissions: 72.9K
+ * Testcase Example:  '[2,5,1,3,4,7]\n3'
+ *
+ * 给你一个数组 nums ，数组中有 2n 个元素，按 [x1,x2,...,xn,y1,y2,...,yn] 的格式排列。
+ * 
+ * 请你将数组按 [x1,y1,x2,y2,...,xn,yn] 格式重新排列，返回重排后的数组。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 输入：nums = [2,5,1,3,4,7], n = 3
+ * 输出：[2,3,5,4,1,7] 
+ * 解释：由于 x1=2, x2=5, x3=1, y1=3, y2=4, y3=7 ，所以答案为 [2,3,5,4,1,7]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 输入：nums = [1,2,3,4,4,3,2,1], n = 4
+ * 输出：[1,4,2,3,3,2,4,1]
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 输入：nums = [1,1,2,2], n = 2
+ * 输出：[1,2,1,2]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= n <= 500
+ * nums.length == 2n
+ * 1 <= nums[i] <= 10^3
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} n
+ * @return {number[]}
+ */
+var shuffle = function(nums, n) {
+  const ret=[]
+  for(let i=0;i<n;i++){
+      ret.push(nums[i])
+      ret.push(nums[i+n])
+  }
+  return ret
+};
+// @lc code=end
+
