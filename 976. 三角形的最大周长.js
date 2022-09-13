d19d23960d6b781349389b0f46164d5ee69929b1
ScarSu <scarsu001@gmail.com>
2022-09-13 13:37:32 +0800

    #976 easy 贪心+排序

---
+++ 976. 三角形的最大周长.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var largestPerimeter = function(nums) {
+    nums.sort((a,b)=>b-a)
+    for(let i=0;i<nums.length-2;i++){
+        if(nums[i]<nums[i+1]+nums[i+2]){
+            return nums[i]+nums[i+1]+nums[i+2]
+        }
+    }
+    return 0
+};
