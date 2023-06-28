baf33b85160a2c29f3718b4d4ea6ec1a98951c2e
ScarSu <scarsu001@gmail.com>
2023-06-28 10:30:17 +0800

    #LCP 02 easy 遍历 辗转相除法求gcd

---
+++ LCP 02. 分式化简.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} cont
+ * @return {number[]}
+ */
+var fraction = function(cont) {
+    // 遍历 用乘法代替除法 计算除数和被除数 1/(a+1/b)=b/(ab+1) 
+    // O(n)
+    const n=cont.length
+    let dividend=cont[n-1]  // 被除数
+    let divisor=1   // 除数
+    for(let i=n-2;i>=0;i--){
+        const temp=divisor
+        divisor=dividend
+        dividend=cont[i]*dividend+temp
+    }
+    // 取除数和被除数的最大公约数
+    const gcd=getGcd(divisor,dividend)
+    return [dividend/gcd, divisor/gcd]
+};
+
+const getGcd=function(a,b){
+    // 辗转相除法
+    if(a<b) [a,b]=[b,a]
+    const mod=a%b
+    if(mod===0) return b
+    return getGcd(b,mod)
+}
